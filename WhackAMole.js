db792f515ce1341b4358bee72ecce4b0217073b3
Asger Phillip Andersen <asgerpandersen@gmail.com>
2020-10-01 23:21:36 +0200

    A lot of base stuff is set up. Game doesn't work yet though

--- WhackAMole.js
+++ WhackAMole.js
@@ -1,25 +1,100 @@
-let width = "1024px";
-let height = "768px";
+// Div elements
+const beginningCircle = document.getElementById("beginningCircle");
+const smallCircle = document.getElementById("smallCircle");
+const mediumCircle = document.getElementById("mediumCircle");
+const bigCircle = document.getElementById("bigCircle");
+const gameWindow = document.getElementById("gameWindowDiv");
 
-let blueBox = document.getElementById("targetDiv");
-//blueBox.style.left = wdith;
+// Window size
+const width = gameWindow.offsetWidth
+const height = gameWindow.offsetHeight
 
-function changePosition() {
-    blueBox.style.background = "green";
+// Variable to control tests
+let testActive = false;
+
+function resetAllTargets() {
+    smallCircle.style.display = "none";
+    mediumCircle.style.display = "none";
+    bigCircle.style.display = "none";
 }
 
-window.onload = changePosition;
+function getRandomIntInRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let r = Math.floor(Math.random() * ((max - 1) + 1)) + min;
+    return r
+}
+
+function calculateCirclePlacement(circleRadius, maxEdge) {
+    let placement = getRandomIntInRange(circleRadius, maxEdge - ((circleRadius * 3) + beginningCircle.offsetWidth));
+    if ((placement + circleRadius) > ((maxEdge / 2) + (beginningCircle.offsetWidth / 2) + circleRadius)) {
+        placement += (beginningCircle.offsetWidth + (2 * circleRadius));
+    }
+    return placement;
+}
+
+function beginRound() {
+    if (!testActive) {
+        testActive = true;
+        let ran = getRandomIntInRange(1, 3);
+        let xPlacement = 0;
+        let yPlacement = 0;
+        switch (ran) {
+            case 1:
+                smallCircle.style.display = "initial";
+                smallCircle.style.position = "absolute";
+                xPlacement = calculateCirclePlacement(smallCircle.offsetWidth / 2, width);
+                yPlacement = calculateCirclePlacement(smallCircle.offsetWidth / 2, height);
+                smallCircle.style.left = xPlacement + 'px';
+                smallCircle.style.top = yPlacement + 'px';
+                break;
+            case 2:
+                mediumCircle.style.display = "initial";
+                mediumCircle.style.position = "absolute";
+                xPlacement = calculateCirclePlacement(mediumCircle.offsetWidth / 2, width);
+                yPlacement = calculateCirclePlacement(mediumCircle.offsetWidth / 2, height);
+                mediumCircle.style.left = xPlacement + 'px';
+                mediumCircle.style.top = yPlacement + 'px';
+                break;
+            case 3:
+                bigCircle.style.display = "initial";
+                bigCircle.style.position = "absolute";
+                xPlacement = calculateCirclePlacement(bigCircle.offsetWidth / 2, width);
+                yPlacement = calculateCirclePlacement(bigCircle.offsetWidth / 2, height);
+                bigCircle.style.left = xPlacement + 'px';
+                bigCircle.style.top = yPlacement + 'px';
+                break;
+        }
+    }
+}
+
+function clickTarget() {
+    resetAllTargets();
+    testActive = false;
+}
+
+function initGame() {
+    beginningCircle.style.display = "inline"
+
+    beginningCircle.onclick = beginRound;
+
+    resetAllTargets();
+    smallCircle.onclick = clickTarget;
+    mediumCircle.onclick = clickTarget;
+    bigCircle.onclick = clickTarget;
+}
+
+window.onload = initGame;
 
 document.onkeydown = onKey;
 
 function onKey(e) {
-    if(e == null) {
+    if (e == null) {
         e = window.event;
     }
     switch (e.which || e.charCode || e.keyCode) {
         case 32:
-            changePosition();
-            console.log(10);
+            beginRound();
             break;
         case 65:
             // a
@@ -29,7 +104,7 @@ function onKey(e) {
             }
             break;
         case 66:
-            // b
-            // here you can extend... alert("pressed the b key"); break;
+        // b
+        // here you can extend... alert("pressed the b key"); break;
     }
 }
